ba83a3fa7beecf8076ba4f35379f810c47d4558d
pokuwagata <pokuwagata@gmail.com>
2024-12-04 11:46:27 +0900

    show url

--- src/Article.tsx
+++ src/Article.tsx
@@ -3,5 +3,10 @@ import { useParams } from "react-router";
 export function Article() {
   const { id } = useParams();
 
-  return <h1>Article {id}</h1>;
+  return (
+    <>
+      <h1>Article {id}</h1>
+      <p>{window.location.href}</p>
+    </>
+  );
 }
